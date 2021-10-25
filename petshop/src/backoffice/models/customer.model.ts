d20d2ff896aff497e88909ee1c7b5b111ed958ae
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-25 00:23:30 -0300

    Add credits card and address's model

--- petshop/src/backoffice/models/customer.model.ts
+++ petshop/src/backoffice/models/customer.model.ts
@@ -1,3 +1,5 @@
+import { Address } from './address.model';
+import { CreditCard } from './credit-card.model';
 import { Pet } from './pet.model';
 
 export class Customer {
@@ -9,6 +11,9 @@ export class Customer {
     public document: string,
     public email: string,
     public pets: Pet[],
+    public billingAddress: Address,
+    public shippingAddress: Address,
+    public creditCard: CreditCard,
     public password: string,
     public active: boolean,
   ) {}
